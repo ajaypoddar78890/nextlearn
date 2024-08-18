f8c8deb4c50b2646de30f3b0113875d1c55d6a77
Your GitHub Username <ajay.poddar15fea@gmail.com>
2024-08-18 23:35:05 +0530

    routing and rendaring from my different side

---
+++ src/routingstatus/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>
+        {" "}
+        this is the new route page for testing purpose in the next js file
+        system and rendaring process all i am doing is copy pasting the code
+        from github and douing it at home
+      </h1>
+    </div>
+  );
+};
+
+export default page;
